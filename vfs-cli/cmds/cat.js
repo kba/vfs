7e352dcc44f3c0ff28ad21dd6f65b511f381010e
Konstantin Baierer <unixprog@gmail.com>
2018-05-03 08:59:47 +0200

    vfs cat

---
+++ vfs-cli/cmds/cat.js
@@ -0,0 +1,15 @@
+const dispatcher = require('@kba/vfs')
+require('@kba/vfs-all-adapters').enableAll(dispatcher)
+
+module.exports = {
+  command: 'cat <location>',
+  aliases: ['tac'],
+  desc: 'cat(1) a vfs node',
+  handler(argv) {
+    const {location} = argv
+    const {path} = dispatcher.parseUrl(location, {slashesDenotesHost: false})
+    const vfs = dispatcher.instantiate('/')
+    vfs.createReadStream(path).pipe(process.stdout)
+  }
+}
+
